48b855cf7761f138b43d7a5707c02ba08b63fbbb
YUUU23 <megan_zheng@brown.edu>
2025-03-20 11:41:58 -0400

    update ext

--- ext/src/index.ts
+++ ext/src/index.ts
@@ -5,34 +5,33 @@ import {
 
 import { ICommandPalette } from '@jupyterlab/apputils';
 
-import { LabShell } from '@jupyterlab/application';
-
 import { NotebookPanel } from '@jupyterlab/notebook';
 
 import { ICodeCellModel } from '@jupyterlab/cells';
 
-function logExecutionNums(app: JupyterFrontEnd) {
-  const labShell = app.shell as LabShell;
-  labShell.currentChanged.connect(() => {
-    const notebook = app.shell.currentWidget as NotebookPanel;
+import { LabShell } from '@jupyterlab/application';
 
-    if (notebook) {
-      notebook.revealed.then(() => {
-        if (notebook.content && notebook.content.model) {
-          const cellList = notebook.content.model.cells;
-          if (cellList != null) {
-            for (const cell of cellList) {
-              console.log('CELL: ', cell, cell.type);
-              if (cell.type === 'code') {
-                const exec_num = (cell as ICodeCellModel).executionCount;
-                console.log('EXECUTION COUNT: ', exec_num);
-              }
-            }
+function currExecutionNums(app: JupyterFrontEnd) {
+  let idToExec = new Map<string, number>();
+  if (app && app.shell && app.shell.currentWidget) {
+    const notebook = app.shell.currentWidget as NotebookPanel;
+    if (
+      notebook.content &&
+      notebook.content.model &&
+      notebook.content.model.cells
+    ) {
+      const cellList = notebook.content.model.cells;
+      for (const cell of cellList) {
+        if (cell.type === 'code') {
+          const exec_num = (cell as ICodeCellModel).executionCount;
+          if (exec_num) {
+            idToExec.set(cell.id, exec_num);
           }
         }
-      });
+      }
     }
-  });
+  }
+  return idToExec;
 }
 
 /**
@@ -43,26 +42,44 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description: 'A JupyterLab extension.',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (
-    app: JupyterFrontEnd,
-    palette: ICommandPalette,
-    notebookPanel: NotebookPanel
-  ) => {
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension ext is activated!');
     console.log('ICP: ', palette);
 
+    let currIdToExec: Map<string, number> = new Map();
+    const labShell = app.shell as LabShell;
+    labShell.currentChanged.connect(() => {
+      const notebook = app.shell.currentWidget as NotebookPanel;
+      if (notebook) {
+        notebook.revealed.then(() => {
+          let out = currExecutionNums(app);
+          console.log('START OUT: ', out);
+          currIdToExec.clear();
+          out.forEach((v, k) => currIdToExec.set(k, v));
+        });
+      }
+    });
+
     const { commands } = app;
     const command = 'Dependencies:get-all-execution';
-
-    logExecutionNums(app);
 
     commands.addCommand(command, {
       label: 'Execution Numbers',
       caption: 'Get all current execution numbers.',
       execute: (args: any) => {
-        console.log(`HELLO`);
+        console.log('Getting all code cells and execution number');
+        let out = currExecutionNums(app);
+        out.forEach((v, k) => {
+          console.log('CELL ID: ', k, ' EXEC NUM: ', v);
+        });
       }
     });
+
+    // On cell execution:
+    //  1. Get execution number from global idToExec map before executing (using cell id)
+    //  2. Get all cells that has an execution number higher than this
+    //  and add the cell ids into a rerun list
+    //  3. Run all cells whose cell id is in the rerun list
 
     const category = 'Dependency Tracking';
     palette.addItem({ command, category, args: { origin: 'from palette' } });
